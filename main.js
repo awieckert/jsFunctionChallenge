e1f0ecd47d0128f938c9e47cfe53216934804399
Adam Wieckert <wiec1369@gmail.com>
2018-03-04 22:18:47 -0600

    Function that checks if number is prime, even and divisible by 10. Prints true or false to DOM.

--- main.js
+++ main.js
@@ -25,3 +25,48 @@ function howOldAmI (birthDate, otherDate){
 }
 
 howOldAmI(2000, 2010);
+
+// Challenge 1 end -------!
+
+
+// Challenge 2 Start -----!
+
+
+function maths(number){
+    var numberProperties = [];
+
+    function isPrime(value) {
+        for(var i = 2; i < value; i++) {
+            if(value % i === 0) {
+                return false
+            }
+        }
+        return true
+    }
+
+    var primeOrNot = isPrime(number);
+    numberProperties.push(primeOrNot);
+
+    if ((number % 2) === 0){
+        numberProperties.push(true);
+    } else if ((number % 2) !== 0) {
+        numberProperties.push(false);
+    }
+
+
+    if ((number % 10) === 0){
+        numberProperties.push(true);
+    } else if ((number % 10) !== 0) {
+        numberProperties.push(false);
+    }
+    
+    var stringToPrint = "<h1>Is " + number + " prime? " + numberProperties[0] + "</h1>";
+    stringToPrint += "<h1>Is " + number + " even? " + numberProperties[1] + "</h1>";
+    stringToPrint += "<h1>Is " + number + " divisible by 10? " + numberProperties[2] + "</h1>";
+    writeToDom(stringToPrint, "challenge-2");
+}
+
+maths(7);
+
+
+// Challenge-2 END------!
